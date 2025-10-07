3ab2feb4e76cd92ff8dc6f41d8c71a7ab63479be
Dhruv Darbha <dhruv.darbha@gmail.com>
2025-10-06 20:08:26 -0400

    change

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn list_example() {
     let mut tail = l.insert(1);
     tail = tail.insert(2);
     tail.insert(3);
-    
+
     println!("List: {}", l);
     l.reverse();
     println!("List reversed: {}", l);
@@ -23,11 +23,11 @@ fn tree_example() {
     let mut t = TreeNode::new();
     let v = vec![0, 1, 2, -1, -2, -3];
     for x in v {
-       println!("=====================");
-       t.insert(x);
-       println!("{}", t.show(3));
-       if !t.validate() {
-         println!("Tree is not balanced!");
-       }
+        println!("=====================");
+        t.insert(x);
+        println!("{}", t.show(3));
+        if !t.validate() {
+            println!("Tree is not balanced!");
+        }
     }
 }
